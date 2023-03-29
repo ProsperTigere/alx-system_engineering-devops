a29aa61186851a83126accba82ca06384852abbd
ProsperTigere <prospertigere2000@gmail.com>
2023-03-29 21:15:16 +0000

    task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
-file {'/tmp/school':
-    mode       => '0744',
-    owner      => www-data,
-    group      => www-data,
-    content    => 'I love Puppet'
-}
+#create a file in /tmp.
+
+file { '/tmp/holberton':
+ensure  => 'present',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',}
